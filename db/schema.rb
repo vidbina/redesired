658cc47c99f9d45599b98515636ba7d02f771631
David Asabina <david@supr.nu>
2014-05-27 00:28:11 +0200

    Setup spree_fancy (generated and migrated)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526155859) do
+ActiveRecord::Schema.define(version: 20140526213540) do
 
   create_table "spree_addresses", force: true do |t|
     t.string   "firstname"
